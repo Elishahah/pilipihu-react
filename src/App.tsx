c109095ddefbb6fd6ca49f7503a30d79e10753ce
Elishah <bstmdtx@126.com>
2021-04-11 23:49:34 +0800

    不满意，准备重写

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import MyHeader from './Header'
+
+import './App.less'
+import './Header.css'
+
+const App = () => (
+  <div className="App">
+    <MyHeader />
+  </div>
+);
+
+export default App;
