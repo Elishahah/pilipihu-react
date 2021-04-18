c279c662d8cbbd812dcdc845332ad0610c2eacf4
Elishah <bstmdtx@126.com>
2021-04-18 20:18:05 +0800

    2021 4 18

---
+++ src/components/footer/MyFooter.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import style from './myfooter.module.css';
+
+interface PropsMyFooter{}
+
+function MyFooter(props: PropsMyFooter){
+    return (
+        <div className={style.footer}>
+
+        </div>
+    );
+}
+
+export default MyFooter;
